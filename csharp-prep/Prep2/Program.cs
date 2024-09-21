086e61088ca598c5ffe899e8989386f0ef8be028
caleb-beardall <calebbeardall@byui.edu>
2024-09-21 12:35:01 -0700

    Completed Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,71 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage? ");
+        string valueInText = Console.ReadLine();
+
+        float gradePercentage = float.Parse(valueInText);
+
+        string letter = "";
+
+        if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+        else if (gradePercentage >= 60 && gradePercentage < 70)
+        {
+            letter = "D";
+        }
+        else if (gradePercentage >= 70 && gradePercentage < 80)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 80 && gradePercentage < 90)
+        {
+            letter = "B";
+        }
+        else
+        {
+            letter = "A";
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class. Better luck next time!");
+        }
+
+        string letterSign = "";
+
+        if (gradePercentage >= 93 || gradePercentage < 60)
+        {
+            letterSign = "";
+        }
+        else if (gradePercentage < 93 && gradePercentage >= 60)
+        {
+            float modulus = gradePercentage % 10;
+
+            if (modulus >= 7)
+            {
+                letterSign = "+";
+            }
+            else if (modulus < 7 && modulus >= 3)
+            {
+                letterSign = "";
+            }
+            else
+            {
+                letterSign = "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{letterSign}");
     }
 }

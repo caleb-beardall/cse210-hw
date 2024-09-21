d026eb40dcadfe7651b76afd71f03be27cffca77
caleb-beardall <calebbeardall@byui.edu>
2024-09-21 14:20:15 -0700

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play = "yes";
+        
+        do
+        {
+            int guessCount = 0;
+
+            int guess;
+
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+
+            Console.WriteLine($"Hint: The number is {number}.");
+
+            do
+            {
+                    Console.Write("What is your guess? ");
+                    string userInput = Console.ReadLine();
+
+                    guess = int.Parse(userInput);
+
+                    guessCount = guessCount + 1;
+
+                    if (number > guess)
+                        {
+                            Console.WriteLine("Higher");
+                        }
+                    else if (number < guess)
+                        {
+                            Console.WriteLine("Lower");
+                        }
+                    else
+                        {
+                            Console.WriteLine("You guessed it!");
+                            Console.WriteLine($"Number of tries: {guessCount}");
+                        }
+
+            }   while (!(number == guess));
+
+            Console.Write("Play again?(yes/no) ");
+            play = Console.ReadLine();
+
+        } while (play == "yes");
+
+        Console.WriteLine("Thank you for playing!");
+    
     }
 }

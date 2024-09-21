fcf6e96f194e91081c71a0fb3004108da2c2fdcb
caleb-beardall <calebbeardall@byui.edu>
2024-09-21 11:16:18 -0700

    Completed Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,21 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    /*
+    What is your first name? Brigham
+    What is your last name? Young
+
+    Your name is Young, Brigham Young.
+    */
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
